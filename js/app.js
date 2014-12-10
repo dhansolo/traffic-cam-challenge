9f1b6f99ef3bab643d2b9f8b564ac5b58f87848c
dhansolo <dhansolo@uw.edu>
2014-12-09 22:16:46 -0800

    Done

--- js/app.js
+++ js/app.js
@@ -6,57 +6,55 @@ $(document).ready(function() {
 		zoom: 12
 	};
 
-	var mapElem = document.getElementById('map');
-	var map = new google.maps.Map(mapElem, mapOptions);
-	var infoWindow = new google.maps.InfoWindow();
-	var markers = [];
+	var mapElement = document.getElementById('map');
+	var map = new google.maps.Map(mapElement, mapOptions);
+	var infoWin = new google.maps.InfoWindow();
+	var cameras = [];
 	var trafficCamera = "1417398990_boot.png";
 	$.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
 		.done(function(data) {
 			data.forEach(function(data) {
 				var position = {
-					lat: Number(data.location.latitude),
-					lng: Number(data.location.longitude)
+					lat: (Number)(data.location.latitude),
+					lng: (Number)(data.location.longitude)
 				};
-				var marker = new google.maps.Marker({
+				var camera = new google.maps.Marker({
 					position: position,
 					map: map,
 					icon: trafficCamera
 				});
-				markers.push(marker);
-				google.maps.event.addListener(marker, 'click', function() {
-					map.panTo(marker.getPosition());
-					var string = '<p>' + data.cameralabel + '<p>';
-					string += '<img src=' + data.imageurl.url + '>';
-					infoWindow.setContent(string);
-					infoWindow.open(map, this);
-					if (marker.getAnimation() != null) {
-						marker.setAnimation(null);
+				cameras.push(camera);
+				google.maps.event.addListener(camera, 'click', function() {
+					map.panTo(camera.getPosition());
+					var string = '<p>' + data.cameralabel + '</p>' +'<img src=' + data.imageurl.url + '>';
+					infoWin.setContent(string);
+					infoWin.open(map, this);
+					if (camera.getAnimation() != null) {
+						camera.setAnimation(null);
 					} else {
-					    marker.setAnimation(google.maps.Animation.BOUNCE);
+					    camera.setAnimation(google.maps.Animation.BOUNCE);
 					    setTimeout(function() {
-					    	marker.setAnimation(null);
+					    	camera.setAnimation(null);
 					    }, 700);
 					}
 				})
 				google.maps.event.addListener(map, 'click', function() {
-					infoWindow.close();
-				})
-				$("search").bind('search keyup'), function() {
+					infoWin.close();
+				});
+				$('#search').bind('search keyup', function() {
 					var search = this.value.toLowerCase();
-					if(!camera.cameralabel.toLowerCase().indexOf(search) < 0) {
-						marker.setMap(map);
+					if(data.cameralabel.toLowerCase().indexOf(search) < 0) {
+						camera.setMap(null);
 					} else {
-						marker.setMap(null);
+						camera.setMap(map);
 					}
-				}
-			});
+				})
+			})
 		})
 		.fail(function(error) {
 			alert("Request Failed");
 		})
 	});
-
 
 
 
